d5889254e067840c57cc9ec8fd9dfe588bf100d1
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-12 21:39:17 +0500

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: [true, "Full name is required"],
+            trim: true,
+        },
+        username: {
+            type: String,
+            required: [true, "Username is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: [true, "Avatar is required"],
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
