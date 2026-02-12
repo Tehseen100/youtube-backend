a8bd81a7f24bc8ebd07b110d9f66a375df5970ce
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-12 21:34:01 +0500

    add error handler middleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Something went wrong. Please try again",
+        errors: err.errors || []
+    })
+}
+
+export default errorHandler;
