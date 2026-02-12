b2da83e20a3c83c1edf6eceaba584e753e7d541d
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-12 21:11:08 +0500

    add configuration and database connection setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes.js"
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// Middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+
+// Error handler middleware
+app.use(errorHandler);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import app from "./app.js";
 
 // Load environment variables
@@ -6,10 +7,17 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.json("Hello World!");
-})
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed: ", error)
+    })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-})
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+// })
