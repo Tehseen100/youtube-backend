a38a8d9f62391630d34927d5c714e225170a2cc7
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-13 16:18:10 +0500

    add multer middleware for file uploads

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import crypto from "crypto";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+    },
+    filename: function (req, file, cb) {
+        const fileName = crypto.randomBytes(12).toString("hex") +
+            path.extname(file.originalname);
+
+        cb(null, fileName);
+    },
+});
+
+export const upload = multer({ storage });
