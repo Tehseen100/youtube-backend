6ae749ade7418eb062f64f91f6667a7ffee4ab9d
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-13 07:22:18 +0500

    add ApiError, ApiResponse and asyncHandler utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+class ApiError extends Error {
+
+    constructor(statusCode, message, errors = []) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.success = false;
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+
+}
+
+export default ApiError;
