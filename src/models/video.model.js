31dc9ecf857a6439bd4f28295d1b41bb53e4090e
Tehseen Javed <tehseenjaved100@gmail.com>
2026-02-17 20:59:42 +0500

    add video model

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: 200
+        },
+        description: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+        // cloudinary url
+        video: {
+            url: { type: String, required: true },
+            public_id: { type: String, required: true }
+        },
+        // cloudinary url
+        thumbnail: {
+            url: { type: String, required: true },
+            public_id: { type: String, required: true }
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
